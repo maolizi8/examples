1d4433b2f2cd4a9165688d4b72191154301ce837
letty <635961515@163.com>
2016-12-07 16:40:11 +0800

    Update sql2excel.py

--- python/records/sql2excel.py
+++ python/records/sql2excel.py
@@ -7,7 +7,31 @@ conn = mc.connect(user='root', password='', host='127.0.0.1', database='maolizi'
 cur = conn.cursor()  
   
 # query language  
-sql = "select name,age from user"  
+#sql = "select name,age from user" 
+sql ="""SELECT issues.issuenumber as '关键字',issues.summary as '摘要',issues.priority as '优先级',issues.status as '状态',issues.fix as '解决',issues.created as '创建日期',userone.assign as '经办人',usertwo.report as '报告人' FROM
+(
+(
+SELECT j.id,j.PROJECT,CONCAT(p.pkey,'-',j.issuenum) as issuenumber,j.SUMMARY,j.ASSIGNEE,j.REPORTER,pi.pname as priority,i.pname as status,r.pname as fix,j.CREATED
+from jiraissue j,project p,resolution r,issuestatus i,priority pi
+where j.PROJECT=p.id
+AND j.PRIORITY=pi.ID
+AND j.issuestatus=i.ID
+AND j.RESOLUTION=r.ID
+AND  j.issuetype=1
+AND (j.PRIORITY=1 OR j.PRIORITY=2)
+AND j.project=12503
+) issues
+LEFT JOIN
+(
+SELECT a.user_key,c.display_name as assign from app_user a,cwd_user c
+WHERE a.lower_user_name=c.lower_user_name
+) userone on issues.ASSIGNEE=userone.user_key
+LEFT JOIN
+(
+SELECT a.user_key,c.display_name as report from app_user a,cwd_user c
+WHERE a.lower_user_name=c.lower_user_name
+) usertwo on issues.REPORTER=usertwo.user_key
+)"""   
 data = cur.execute(sql)  
 index = cur.description 
 
