16ab35e63d6ff5529dac3ecd2aa59de353582b22
maolizi8 <635961515@163.com>
2017-02-23 18:09:39 +0800

    201702231809

--- js/learn.js
+++ js/learn.js
@@ -1,6 +1,7 @@
 $(function() {
 	var header = '<div class="header" id="header">';
-	header += '<div class="title"><h1><a href="index.html">liz学习笔记</a></h1></div>';
+	header += '<div class="title"><a href="../index.html" class="liz_index">liz学习空间</a>';
+	header += '<a href="index.html" class="liz_menu">学习笔记</a></div>';
 	header += '<div class="header-r fade">';
 	header += '<button class="menu-btn" onclick="dispMenuSection()">menu</button>';
 	header += '</div></div><div id="nav"></div>';
@@ -12,18 +13,9 @@ $(function() {
 });
 $(".h_aside_list li").click(function() {
 	var that = this;
-	$(".h_aside_list li").css({
-		"background-color": "#fff",
-		"color": "#000"
-	});
-	$(that).css({
-		"background-color": "cornflowerblue",
-		"color": "#fff"
-	});
+	$(".h_aside_list li").removeClass("li_active");
+	$(that).addClass("li_active");
 	document.title="liz学习笔记-"+$(that).text();
-});
-
-$(".h_aside_list li").on("click", function() {
 	$(".main").css("display", "none");
 	$("#" + $(this).attr("data-target")).css("display", "block");
 });
--- js/pagetransf.js
+++ js/pagetransf.js
@@ -1,4 +1,37 @@
 var paginator='<div class="pagenator">';
 paginator+='<button class="prepage" onclick="prePage()">&larr;上一页</button>';
-paginator+='<button class="nextpage" onclick="nextpage()">下一页&rarr;</button></div>';
+paginator+='<button class="nextpage" onclick="nextPage()">下一页&rarr;</button></div>';
 $(".main").append(paginator);
+
+var liLen=$(".h_aside_list li").length;
+	
+function currListLi(){
+	var i;
+	for (i=0;i<liLen;i++) {
+		if ($($(".h_aside_list li")[i]).hasClass("li_active")) {
+			return i;
+		}
+	}
+}
+
+function prePage(){
+	var prePage=currListLi()-1;
+	if(prePage>=0){
+		console.log(prePage);
+		$(".h_aside_list li").removeClass("li_active");
+		$($(".h_aside_list li")[prePage]).addClass("li_active");
+		$(".main").css("display", "none");
+		$($(".main")[prePage]).css("display", "block");
+		$(window).scrollTop(0);
+	}
+}
+function nextPage(){
+	var nextPage=currListLi()+1;
+	if(nextPage<liLen){
+		$(".h_aside_list li").removeClass("li_active");
+		$($(".h_aside_list li")[nextPage]).addClass("li_active");
+		$(".main").css("display", "none");
+		$($(".main")[nextPage]).css("display", "block");
+		$(window).scrollTop(0);
+	}
+}
